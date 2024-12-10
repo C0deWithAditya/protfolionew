a1aa13bff985e1694ccaba5cb4566edc6398565f
Aditya Verma <adityagen.lko12@gmail.com>
2024-12-10 14:19:34 +0530

    push new

---
+++ client/src/Config/Config.js
@@ -0,0 +1,260 @@
+import { BiCodeAlt } from 'react-icons/bi';
+import { IoBrushOutline } from 'react-icons/io5';
+
+export const SkillSet=[
+    {
+        id: "1",
+        name: "C",
+        imgsrc: "images/C++.png"
+    },
+    {
+        id: "2",
+        name: "Java",
+        imgsrc: "images/java.png"
+    },
+    {
+        id: "3",
+        name: "MySql",
+        imgsrc: "images/mysql.png"
+    },
+    {
+        id: "4",
+        name: "Bootstrap",
+        imgsrc: "images/Bootstrap.png"
+    },
+    {
+        id: "5",
+        name: "HTML",
+        imgsrc: "images/html5.png"
+    },
+   
+    {
+        id: "6",
+        name: "CSS",
+        imgsrc: "images/css3.png"
+    },
+    {
+        id: "7",
+        name: "Javascript",
+        imgsrc: "images/javascript.png"
+    }, 
+    {
+        id: "8",
+        name: "React Js",
+        imgsrc: "images/react.png"
+    },
+    {
+        id: "9",
+        name: "PHP",
+        imgsrc: "images/php.png"
+    },
+    {
+        id: "10",
+        name: "XAMPP",
+        imgsrc: "images/XAMPP.png"
+    },
+    {
+        id: "11",
+        name: "Github",
+        imgsrc: "images/github.png"
+    },
+    {
+        id: "12",
+        name: "Tailwind CSS",
+        imgsrc: "images/tailwind-css.png"
+    },
+    {
+        id: "13",
+        name: "JSP",
+        imgsrc: "images/JSP.png"
+    },
+    {
+        id: "14",
+        name: "AWS",
+        imgsrc: "images/AWS.png"
+    },
+    {
+        id: "15",
+        name: "ChatGPT",
+        imgsrc: "images/ChatGPT.png"
+    },
+    {
+        id: "16",
+        name: "Netlify",
+        imgsrc: "images/netlify.png"
+    },
+    {
+        id: "17",
+        name: "AI",
+        imgsrc: "images/artificial.png"
+    },
+    
+    {
+        id: "18",
+        name: "ApacheNetBeans",
+        imgsrc: "images/ApacheNetBeans.png"
+    },
+      
+    {
+        id: "19",
+        name: "VSCode",
+        imgsrc: "images/VScode.png"
+    },
+    {
+        id: "20",
+        name: "Canva",
+        imgsrc: "images/canva.png"
+    },
+    
+   
+   
+    /*
+    {
+        id: "22",
+        name: "Figma",
+        imgsrc: "images/figma.png"
+    }, 
+    {
+        id: "19",
+        name: "Illustrator",
+        imgsrc: "images/Illustrator.png"
+    }, */
+
+    
+]
+
+export const ProjImg = [
+    {
+        id: "1",
+        name: "Global News Website",
+        category: "react",
+        imgsrc: "projectImg/react/News.png",
+        href: "https://github.com/C0deWithAditya/newapp.github.io",
+        desc:"This is a news website which is created using ReactJs.This website uses API to fetch news ,Weather and Stock Market News globally."
+    },
+    {
+        id: "2",
+        name: "Static Car Selling Website",
+        category: "javascript",
+        imgsrc: "projectImg/react/car Selling.png",
+        href: "https://c0dewithaditya.github.io/Car.github.io/",
+        desc: "The ultimate destination for buying, servicing, and repairing cars with top-notch expertise and reliable solutions.I had created this project in post graduation(2023)and Development time-7 Days."
+    },
+    {
+        id: "3",
+        name: "Textutils",
+        category: "react",
+        imgsrc: "projectImg/react/textutils.png",
+        href: "https://c0dewithaditya.github.io/textutils/",
+        desc: "This is a TextUtils which counting words and characters, our online editor can help you to improve word choice and writing style, and, optionally, help you to detect grammar mistakes and plagiarism,created using React JS and  its development time-6 Day."
+    },
+    
+    {
+        id: "7",
+        name: "Tic tac toe",
+        category: "javascript",
+        imgsrc: "projectImg/react/Tic.jpg",
+        href: "https://c0dewithaditya.github.io/tic-tac-toe.github.io/",
+        desc: "This is a tic-tac-toe game which is created using html,css and javascript language and  development time-1 Day."
+    },
+    {
+        id: "8",
+        name: "OmniFood Static Website",
+        category: "javascript",
+        imgsrc: "projectImg/react/foodweb.png",
+        href: "https://c0dewithaditya.github.io/food.github.io/",
+        desc: "I have created a food website using html,css and javascript language in Graduation Project(2021)(Development time-15 Days)"
+    },
+
+    {
+        id: "10",
+        name: "Carbooking Website",
+        category: "javascript",
+        imgsrc: "projectImg/react/carbookingnew.png",
+        href: "https://c0dewithaditya.github.io/CarRentingAdvance.github.io/",
+        desc: "Car Renting Website is created using html,css and javascript language with Responsive design.(Development time-5 Days)"
+    },
+    {
+        id: "11",
+        name: "Major Car Renting Website",
+        category: "javascript",
+        imgsrc: "projectImg/react/majorcar.png",
+        href: "https://github.com/C0deWithAditya/carRental",
+        desc: "Discover the ultimate destination for buying and repairing cars with top-notch expertise and reliable solutions.This website is created using html,css and javascript language and backend using Java in Post Graduation Minor Project(2024) and Development time-10 Days."
+    },
+    {
+        id: "12",
+        name: "Hospital Management Website",
+        category: "javascript",
+        imgsrc: "projectImg/react/Hospital.png",
+        href: "https://github.com/C0deWithAditya/carRental",
+        desc: "This is major project which is created using html,css and javascript language and backend using php  in Post Graduation Major Project(2024) and Development time-15 Days. "
+    },
+    ]
+export const Experience = [
+    {
+        id: "1",
+        date: "2022 - 2 Month",
+        name: "YEP Training",
+        company: "Tata Consultancy Services",
+        desc: ["Self Development Training."]
+    },
+    {
+        id: "2",
+        date: "08/2023 – 09/2023",
+        name: "Web Developement Summer Internship",
+        company: "DigiCoders, Lucknow,Uttar Pradesh",
+        desc: ["Mastered front-end and back-end technologies(JAVA)during a web development certification course with A++ grade."]
+    },
+    {
+        id: "3",
+        date: "02/2022 – 05/2022",
+        name: "Web Developement Internship",
+        company: "Live It Solutions, Lucknow,Uttar Pradesh",
+        desc: ["Mastered front-end and back-end technologies(PHP) during UnderGraduation course resulting in the successful design of responsive web applications."]
+    },
+    
+]
+export const Education=[
+    {
+        id: 1,
+        date: "2022-2024",
+        name: "MCA",
+        company: "Shri Ramswaroop College Of Engineering and Management, Lucknow,Uttar Pradesh",
+        desc: "Master's Course in Computer Applications."
+    },
+    {
+        id: 2,
+        date: "2019-June-2022",
+        name: "B.VOC(Software Developrment & E-Governance)",
+        company: "National PG College,Lucknow,Uttar Pradesh",
+        desc: "Completed B.VOC Degree in Software Developrment & E-Governance."
+    },
+    {
+        id: 3,
+        date: "2018-2019",
+        name: "XIIth",
+        company: "City Montessori School, Lucknow, Uttar Pradesh",
+        desc: "Science – ISC Board."
+    },
+    {
+        id: 4,
+        date: "2016-2017",
+        name: "Xth",
+        company: "City Montessori School, Lucknow, Uttar Pradesh",
+        desc: "Science – ICSE Board."
+    },
+    
+]
+export const Services=[
+    {
+        id: 1,
+
+         name: "Full-Stack Developer",
+         desc: "Develop creative and responsive Website for your business.",
+    },
+    {   id: 2,
+        name: "Software Developer",
+        desc: "Can Build a Software solution for your business.",
+    }
+]
